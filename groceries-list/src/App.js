962e76f6918d9e3816b2cb4e29e6a765c4345ec8
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-07-03 16:32:36 -0400

    added files

---
+++ groceries-list/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Data from './data/GroceryData'
+
+import { Component } from 'react'
+
+class App extends Component {
+
+state= {
+  grocerylist: grocerylist
+}
+
+
+
+render() {
+  console.log(grocerylist);
+  return(
+    <div>
+      <h1>Grocery List</h1>
+
+      <div className='container'>
+        
+      </div>
+    </div>
+  )
+}
+}
---
+++ groceries-list/src/data/GroceryData.js
@@ -0,0 +1,59 @@
+const groceries = [
+  {
+    item: 'salads',
+    brand: 'GreatValue',
+    units: '',
+    quantity: 4,
+    isPurchased: false
+  },
+
+  {
+    item: 'Poptarts',
+    brand: 'GreatValue',
+    units: '2',
+    quantity: 2,
+    isPurchased: false
+  },
+
+  {
+    item: 'Organic Mangoes',
+    brand: 'Dole',
+    units: '',
+    quantity: 5,
+    isPurchased: false
+  },
+
+  {
+    item: 'Pops Cereal',
+    brand: 'Kellogs',
+    units: '',
+    quantity: 1,
+    isPurchased: false
+  },
+
+  {
+    item: 'Brown Rice',
+    brand: 'Tyson',
+    units: '',
+    quantity: 1,
+    isPurchased: false
+  },
+
+  {
+    item: 'Water',
+    brand: 'Smart Water',
+    units: '',
+    quantity: 8,
+    isPurchased: false
+  }
+
+]
+    
+  export default groceries;
+  
+  
+  
+  
+  
+  
+  
